f63ee6b72fb9ce2b090ef81299b354072821d242
Kapino <Kapian1234@users.noreply.github.com>
2022-04-27 21:29:30 +0800

    Updates

---
+++ precache-manifest.82353ef4cc3cabe6299a292191be6a87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f39f81ccec8216bc652",
+    "url": "/To-do-List/static/css/main.fd120ce8.chunk.css"
+  },
+  {
+    "revision": "5f39f81ccec8216bc652",
+    "url": "/To-do-List/static/js/main.5f39f81c.chunk.js"
+  },
+  {
+    "revision": "23bd69898d45e2216234",
+    "url": "/To-do-List/static/js/1.23bd6989.chunk.js"
+  },
+  {
+    "revision": "904e15de124dc40f99d4",
+    "url": "/To-do-List/static/js/runtime~main.904e15de.js"
+  },
+  {
+    "revision": "6d985fb8b0b3862be9b23c64e2280a1e",
+    "url": "/To-do-List/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/To-do-List/precache-manifest.82353ef4cc3cabe6299a292191be6a87.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/To-do-List/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
