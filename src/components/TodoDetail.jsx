5447659976bbc54a4163eb7c3349b1384a526d95
Kapino <45655760+Kapian1234@users.noreply.github.com>
2022-04-29 21:51:43 +0800

    修复完成时间显示错误

--- src/components/TodoDetail.jsx
+++ src/components/TodoDetail.jsx
@@ -27,7 +27,7 @@ function TodoDetail() {
           <div className='detail-info '>
             <div className='hvr-forward'>ID: {todo.id}</div><br/>
             <div className='hvr-forward'>创建于 {yAdded}年{mAdded}月{dAdded}日 {hAdded<10?'0'+hAdded.toString():hAdded}:{minAdded<10?'0'+minAdded.toString():minAdded}</div>
-            <div>{todo.isComplete?<div>完成于 {yComplete}年{mComplete}月{dComplete}日 {hAdded<10?'0'+hAdded.toString():hAdded}:{minComplete<10?'0'+minComplete.toString():minComplete}</div>:<div className='hvr-forward'>未完成</div>}</div>        
+            <div>{todo.isComplete?<div>完成于 {yComplete}年{mComplete}月{dComplete}日 {hComplete<10?'0'+hComplete.toString():hComplete}:{minComplete<10?'0'+minComplete.toString():minComplete}</div>:<div className='hvr-forward'>未完成</div>}</div>        
           </div>
           
         </div>
